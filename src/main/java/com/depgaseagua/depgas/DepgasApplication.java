13fbcba015063f1823691eb091fb1cdc4c4d3d87
Jorge Mamoni <jmamonip@gmail.com>
2024-02-22 15:03:54 -0300

    Projet criado

---
+++ src/main/java/com/depgaseagua/depgas/DepgasApplication.java
@@ -0,0 +1,13 @@
+package com.depgaseagua.depgas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepgasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepgasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/depgaseagua/depgas/DepgasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.depgaseagua.depgas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepgasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
