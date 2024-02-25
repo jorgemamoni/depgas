182e3602bb789b4ccddbca9876580e82be3d8c1d
Jorge Mamoni <jmamonip@gmail.com>
2024-02-25 10:12:44 -0300

    User insert

--- src/main/java/com/depgaseagua/depgas/resources/UserResource.java
+++ src/main/java/com/depgaseagua/depgas/resources/UserResource.java
@@ -1,17 +1,20 @@
 package com.depgaseagua.depgas.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.depgaseagua.depgas.entities.User;
 import com.depgaseagua.depgas.services.UserService;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
@@ -33,4 +36,10 @@ public class UserResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
 }
